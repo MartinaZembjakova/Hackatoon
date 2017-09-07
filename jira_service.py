b640a685c3bd15490f54b3fbfb5274f2c8bfadbf
jcechak <jarekcechak@gmail.com>
2017-09-07 15:26:54 +0200

    Initial settings and JIRA API calls.

--- jira_service.py
+++ jira_service.py
@@ -1,3 +1,15 @@
 """
 This file contains all the necessary functions and handlers for JIRA integration.
 """
+import requests
+
+from settings import JIRA_USERNAME, JIRA_PASSWORD, JIRA_URL
+
+
+def inicalize_session():
+    session = requests.Session()
+    credentials = {"username": JIRA_USERNAME, "password": JIRA_PASSWORD}
+    session.post('https://jira-stage.solarwinds.com/rest/auth/1/session', json=credentials)
+    return session
+
+
--- settings.py
+++ settings.py
@@ -1,3 +1,14 @@
 """
 This file contains application settings and default presets.
 """
+import os
+
+# Skype credentials and settings
+SKYPE_USENAME = os.environ.get("skype_username")
+SKYPE_PASSWORD = os.environ.get("skype_password")
+CHAT_INVITE_URL = os.environ.get("invite_url")
+
+# Jira credentials and settings
+JIRA_USERNAME = os.environ.get("jira_username")
+JIRA_PASSWORD = os.environ.get("jira_password")
+JIRA_URL = os.environ.get("jira_url")
