c480ab9a731183bb314abfba5f61aee08abc3502
jcechak <jarekcechak@gmail.com>
2017-09-07 23:03:32 +0200

    Refactoring, PEP8 and docstrings.

--- main.py
+++ main.py
@@ -2,21 +2,20 @@
 This file contains main function for running the application itself.
 """
 from datetime import datetime, timedelta
-from time import sleep
 import threading
 
-from msg_formatter import format_message
-import jira_service
+from message_formatter import format_message
+from jira_service import initialise_session, list_new_issues, close_session, parse_issue
 from settings import SKYPE_USERNAME, SKYPE_PASSWORD, CHAT_INVITE_URL, PROJECT_NAME, TIMEOUT
-from skype_service import sendMessage, openChat, logIn
+from skype_service import send_message, open_chat, login
 
 
 def main():
     # initialise sessions and establish connections
-    skype = logIn(SKYPE_USERNAME, SKYPE_PASSWORD)
-    skype_chat = openChat(skype, CHAT_INVITE_URL)
+    skype = login(SKYPE_USERNAME, SKYPE_PASSWORD)
+    skype_chat = open_chat(skype, CHAT_INVITE_URL)
     last_check = datetime.now() - timedelta(1)
-    session = jira_service.initialise_session()
+    session = initialise_session()
 
     # stopping flag
     stop_event = threading.Event()
@@ -30,10 +29,18 @@ def main():
     stop_event.set()
     checking_thread.join()
 
-    jira_service.close_session(session)
+    close_session(session)
 
 
 def repeat_checks(last_check, session, skype_chat, stop_event):
+    """
+    Repeatedly runs checks for new issues.
+    :param last_check: date of the last check
+    :param session: http session for JIRA API calls
+    :param skype_chat: chat to which the messages will be sent
+    :param stop_event: flag indicating user will to close the program
+    :return: None
+    """
     while not stop_event.is_set():
         new_last_check = datetime.now()
         do_check(last_check, session, skype_chat)
@@ -42,11 +49,18 @@ def repeat_checks(last_check, session, skype_chat, stop_event):
 
 
 def do_check(last_check, session, skype_chat):
-    issues = jira_service.list_new_issues(session, last_check, PROJECT_NAME)
+    """
+    Does a single check for new issues.
+    :param last_check: date of the last check
+    :param session: http sesion for JIRA API calls
+    :param skype_chat: chat to which the messages will be sent
+    :return: None
+    """
+    issues = list_new_issues(session, last_check, PROJECT_NAME)
     for issue in issues:
-        case, summary, issue_type, status = jira_service.parse_issue(issue)
+        case, summary, issue_type, status = parse_issue(issue)
         message = format_message(case, summary, issue_type, status)
-        sendMessage(skype_chat, message)
+        send_message(skype_chat, message)
 
 
 def user_input():
--- skype_service.py
+++ skype_service.py
@@ -4,15 +4,35 @@ This file contains all the necessary functions and handlers for Skype integratio
 
 from skpy import Skype
 
-def logIn(username, password):
+
+def login(username, password):
+    """
+    Logs a Skype user in.
+    :param username: user name of the user to be logged in
+    :param password: password for the user to be logged in
+    :return: Skype connection object
+    """
     return Skype(username, password)
-    print("JIRA logged in")
 
-def openChat (sk, chatUrl):
-    id = sk.chats.urlToIds(chatUrl)
-    sk.chats.sync()
-    ch = sk.chats[id["id"]]
-    return ch
 
-def sendMessage(chat, msg):
-    chat.sendMsg(msg)
+def open_chat(skype, chat_url):
+    """
+    Opens chat with the given invite URL.
+    :param skype: Skype connection object
+    :param chat_url: URL of chat that will be used to sent messages into
+    :return: Skype chat object
+    """
+    chat_id = skype.chats.urlToIds(chat_url)
+    skype.chats.sync()
+    chat = skype.chats[chat_id["id"]]
+    return chat
+
+
+def send_message(chat, message):
+    """
+    Send the given message to the given chat.
+    :param chat: Skype chat object
+    :param message: string message
+    :return: None
+    """
+    chat.sendMsg(message)
