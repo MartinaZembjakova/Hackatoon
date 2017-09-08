b740aa2db905312f14b481bc42d3cb25aff4e742
root <root@THYBLOVA-LT.tul.solarwinds.net>
2017-09-08 09:46:35 +0200

    fixing logs

--- settings.py
+++ settings.py
@@ -12,10 +12,13 @@ CHAT_INVITE_URL = os.environ.get("CHAT_INVITE_URL")
 JIRA_USERNAME = os.environ.get("JIRA_USERNAME")
 JIRA_PASSWORD = os.environ.get("JIRA_PASSWORD")
 JIRA_URL = os.environ.get("JIRA_URL")
-MAX_RESULTS = os.environ.get("MAX_RESULTS", 50)
+MAX_RESULTS = int(os.environ.get("MAX_RESULTS", "50"))
 PROJECT_NAME = os.environ.get("PROJECT_NAME", "Log & Event Manager")
-JIRA_SSL_CERTIFICATE = os.environ.get("JIRA_SSL_CERTIFICATE", True)
 ISSUE_TYPES = os.environ.get("ISSUE_TYPES", ("Bug", "Story"))
+JIRA_SSL_CERTIFICATE = os.environ.get("JIRA_SSL_CERTIFICATE", True)
+if JIRA_SSL_CERTIFICATE == "False":
+    JIRA_SSL_CERTIFICATE = False
+
 
 # Others settings
-TIMEOUT = os.environ.get("TIMEOUT", 300)
+TIMEOUT = int(os.environ.get("TIMEOUT", "300"))
