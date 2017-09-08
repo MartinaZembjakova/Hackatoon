42dbf4d4688a84196af366884240fbe9254d60cb
root <root@THYBLOVA-LT.tul.solarwinds.net>
2017-09-08 08:19:55 +0200

    messages for other issue types

--- message_formatter.py
+++ message_formatter.py
@@ -14,8 +14,14 @@ def closed(case, summary):
     return "(movie) " + case + " - " + summary + "\n" + BROWSE_URL + case
 
 
+def other_types(case, summary):
+    return case + " - " + summary + "\n" + BROWSE_URL + case
+
+
 def format_message(case, summary, issue_type, status):
-    if status == 'Open':
+    if status == 'Open' and issue_type == "Bug":
         return bug(case, summary)
+    elif status == 'Open':
+        return other_types(case, summary)
     else:
         return closed(case, summary)
